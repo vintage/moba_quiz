21f2b09ea5c9464921095b141e3229dde10a1100
Kamil Rykowski <kamil.rykowski@blueservices.pl>
2016-01-16 10:14:47 +0100

    Add cordova email-plugin for asking the questions

--- app/main_menu/page.ts
+++ app/main_menu/page.ts
@@ -1,5 +1,5 @@
 import {OnInit} from 'angular2/core';
-import {Button, Page, NavController} from 'ionic-framework/ionic';
+import {Button, Page, NavController, Platform} from 'ionic-framework/ionic';
 
 import {GamePage} from "../game/game";
 import {GameplayService} from "../gameplay/service";
@@ -13,8 +13,9 @@ export class MainMenuPage {
   public timesPlayed:number;
   public bestScore:number;
 
-  constructor(nav: NavController, gameplayService: GameplayService, scoreService: ScoreService) {
+  constructor(nav: NavController, gameplayService: GameplayService, scoreService: ScoreService, platform: Platform) {
     this.nav = nav;
+    this.platform = platform;
     this.gameplay = gameplayService;
     this.scoreService = scoreService;
   }
@@ -37,11 +38,46 @@ export class MainMenuPage {
 
   }
 
-  openSettings() {
+  openRating() {
+    this.platform.ready().then(() => {
+      var marketUrl = null;
 
+      console.log(this.platform);
+
+      if(this.platform.is('ios')) {
+        marketUrl = 'itms-apps://itunes.apple.com/app/idYOUR_APP_ID';
+      }
+      else if(this.platform.is('android')) {
+        marketUrl = 'market://details?id=com.YOUR.PACKAGENAME';
+      }
+      else if(this.platform.is('windowsphone')) {
+        marketUrl = 'http://windowsphone.com/s?appId=c14e93aa-27d7-df11-a844-00237de2db9e';
+      }
+
+      if(marketUrl) {
+        window.open(marketUrl);
+      }
+    });
+
+    var devicePlatform = device.platform;
+
+    // Check which platform
+    if (devicePlatform == "iOS") {
+        window.open('https://itunes.apple.com/us/app/YOUR-APP-SLUG-HERE/id000000000?mt=8&uo=4'); // or itms://
+    } else if (devicePlatform == "Android") {
+        window.open('market://details?id=com.YOUR.PACKAGENAME');
+    } else if (devicePlatform == "BlackBerry"){
+        window.open('http://appworld.blackberry.com/webstore/content/<applicationid>');
+    }
   }
 
   openContact() {
-
+    this.platform.ready().then(() => {
+      cordova.plugins.email.isAvailable(function (isAvailable) {
+        if(isAvailable) {
+          cordova.plugins.email.open();
+        }
+      });
+    });
   }
 }
