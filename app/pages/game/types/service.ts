292a91aff913b21f9e91569c18e94cad5fd6213b
Kamil Rykowski <kamil.rykowski@gmail.com>
2016-03-01 20:57:34 +0100

    Adjust styles for iPhone4

--- app/pages/game/types/service.ts
+++ app/pages/game/types/service.ts
@@ -1,7 +1,7 @@
-import {Injectable} from 'angular2/core';
-import {sample} from 'lodash';
+import {Injectable} from "angular2/core";
+import {sample} from "lodash";
 
-import {GameTypeModel} from './model';
+import {GameTypeModel} from "./model";
 import {ItemRecipeGame} from "./item_recipe/component";
 import {ItemPriceGame} from "./item_price/component";
 import {ChampionSkillsGame} from "./champion_skills/component";
@@ -14,11 +14,11 @@ export class GameTypeService {
 
   constructor() {
     this.gameTypes = [
-      new GameTypeModel('item_recipe', ItemRecipeGame),
-      new GameTypeModel('item_price', ItemPriceGame),
-      new GameTypeModel('champion_skills', ChampionSkillsGame),
-      new GameTypeModel('champion_attack_type', ChampionAttackTypeGame),
-      new GameTypeModel('skill_champion', SkillChampionGame)
+      new GameTypeModel("item_recipe", ItemRecipeGame),
+      new GameTypeModel("item_price", ItemPriceGame),
+      new GameTypeModel("champion_skills", ChampionSkillsGame),
+      new GameTypeModel("champion_attack_type", ChampionAttackTypeGame),
+      new GameTypeModel("skill_champion", SkillChampionGame)
     ];
   }
 
