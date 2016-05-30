303b95b78aa29088cb2e5604ad36a3a27838aa06
Kamil Rykowski <kamil.rykowski@gmail.com>
2016-05-30 20:01:42 +0200

    Remove progress bar & replace it with integer timer (performance boots due to 1000ms interval instead of 100ms)

--- app/pages/game/stats/component.ts
+++ app/pages/game/stats/component.ts
@@ -2,7 +2,7 @@ import {Component, EventEmitter, OnInit} from "angular2/core";
 import {Icon} from "ionic-angular";
 
 import {GameplayService} from "../../../providers/gameplay/service";
-import {PointsPipe} from "../../../pipes/numbers";
+import {PointsPipe, TimeLeftPipe} from "../../../pipes/numbers";
 
 @Component({
   selector: "game-stats",
@@ -10,16 +10,16 @@ import {PointsPipe} from "../../../pipes/numbers";
   directives: [Icon],
   inputs: ["timerEnabled"],
   outputs: ["timeOver"],
-  pipes: [PointsPipe]
+  pipes: [PointsPipe, TimeLeftPipe]
 })
 export class Stats implements OnInit {
   timeOver = new EventEmitter();
 
-  progressMax: number;
+  progressCur: number;
   timerEnabled: boolean;
 
   constructor(public gameplay: GameplayService) {
-    this.progressMax = 1000;
+    this.progressCur = this.gameplay.timeLimit / 1000;
     this.timerEnabled = true;
   }
 
@@ -27,17 +27,15 @@ export class Stats implements OnInit {
     this.updateTimer();
   }
 
-  currentProgress() {
-    return (this.gameplay.timeLeft / this.gameplay.timeLimit) * this.progressMax;
-  }
-
   updateTimer() {
-    let interval = 100;
+    let interval = 1000;
 
     setTimeout(() => {
       if (this.timerEnabled) {
         this.gameplay.timeLeft -= interval;
       }
+
+      this.progressCur = Math.max(this.gameplay.timeLeft / 1000, 0);
 
       if (this.gameplay.timeLeft === 0) {
         this.timeOver.emit(null);
--- app/pipes/numbers.ts
+++ app/pipes/numbers.ts
@@ -22,3 +22,19 @@ export class PointsPipe implements PipeTransform {
     return result.split("").reverse().join("");
   }
 }
+
+@Pipe({name: "timeLeft"})
+export class TimeLeftPipe implements PipeTransform {
+  transform(value: number, args: string[]): any {
+    if (isNaN(value)) {
+      return value;
+    }
+
+    let stringValue = value.toString();
+    if (stringValue.length > 1) {
+      return stringValue;
+    }
+
+    return "0" + stringValue;
+  }
+}
