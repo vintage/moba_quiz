ecc3125a9444237663122b4fbb7761005c564685
Kamil Rykowski <kamil.rykowski@gmail.com>
2016-11-12 17:39:41 +0100

    [VG] Initial vainglory parser

--- parsers/app/vainglory/parser.py
+++ parsers/app/vainglory/parser.py
@@ -2,35 +2,25 @@ import json
 import os
 import shutil
 import functools
-import time
+import urllib.request
 
 from PIL import Image
 import requests
 from tqdm import tqdm
 from pyquery import PyQuery as pq
 
-
 item_image_path = './data/images/items/'
 champion_image_path = './data/images/champions/'
 
-items_url = 'https://www.smitegame.com/items/'
-champions_url = 'https://www.smitegame.com/gods/'
+items_url = 'http://vainglorydb.com/items'
+champions_url = 'http://vainglorydb.com/heroes'
 
 os.makedirs(item_image_path, exist_ok=True)
 os.makedirs(champion_image_path, exist_ok=True)
 
 
 def clean_filename(filename):
-    filename = ''.join(filename.split()).lower()
-    extension_dot = filename.rindex('.')
-
-    left_part = filename[:extension_dot]
-    right_part = filename[extension_dot:]
-
-    left_part = ''.join([c for c in left_part if c.isalnum()])
-    right_part = ''.join([c for c in right_part if c.isalpha()])
-
-    return "{}.{}".format(left_part, right_part)
+    return ''.join(filename.split()).lower()
 
 
 def download_image(url, path, filename):
@@ -49,114 +39,118 @@ def download_image(url, path, filename):
     return filename
 
 
+def download_champion_image(url, path, filename):
+    filename = clean_filename(filename)
+    full_path = os.path.join(path, filename)
+
+    # download image
+    urllib.request.urlretrieve(url, full_path)
+
+    # compress image
+    image = Image.open(full_path)
+    image.save(full_path, quality=95, optimize=True)
+
+    return filename
+
+
 def setup_items():
     dom = pq(url=items_url)
-    container = dom('.god-container')
+    container = dom('.items')
 
     result = []
-    for item in tqdm(container.items('.item-package'), desc='Parsing items'):
-        name = item.find('.title').text()
-        price = int(item.find('.cost').text())
-        start_from = item.find('.starting-item-name').text()
+    for item in tqdm(container.items('li'), desc='Parsing items'):
+        detail_url = item.find('a').attr('href')
+        detail_dom = pq(url=detail_url)
 
-        # For SMITE we want to import only base items (tier 1)
-        if name not in start_from:
-            continue
+        data = json.loads(item.attr('data-item'))
 
-        image_url = item.find('img.icon').attr('src')
-        image_name = image_url.split('/')[-1]
+        item_id = data['id']
+        item_name = data['name']
+        item_price = data['cost']
+
+        image_url = data['icon']
+        image_name = 'item_{}.png'.format(item_id)
+
+        build_trees = list(detail_dom.items('.build-tree'))
+
+        recipe = []
+        for build_tree in build_trees:
+            recipe_items = [
+                json.loads(i.attr('data-item')) for i in list(build_tree.items('a'))
+            ]
+
+            recipe_ids = [r['id'] for r in recipe_items]
+            if item_id in recipe_ids:
+                continue
+
+            if item_price == sum([r['cost'] for r in recipe_items]):
+                recipe = recipe_ids
+                break
 
         result.append({
-            'id': name.lower(),
-            'name': name,
+            'id': item_id,
+            'name': item_name,
             'image': download_image(image_url, item_image_path, image_name),
             'into': [],
-            'from': [],
-            'price': price,
+            'from': recipe,
+            'price': item_price,
         })
 
     with open('./data/items.json', 'w') as outfile:
-        json.dump(result, outfile)
+        json.dump(result, outfile, ensure_ascii=False)
 
     return result
 
 
 def setup_champions():
     dom = pq(url=champions_url)
-    container = dom('.god-container')
-
-    god_rows = pq(url='http://smite.gamepedia.com/List_of_gods').items('table tr')
+    container = dom('.items')
 
     result = []
-    for item in tqdm(container.items('.god-package'), desc='Parsing champions'):
+    for item in tqdm(container.items('li'), desc='Parsing champions'):
         detail_url = item.find('a').attr('href')
+        detail_dom = pq(url=detail_url)
 
-        name = item.find('.god-name').text()
+        champion_name = detail_dom.find('.section-head a').text()
+        champion_id = champion_name.lower()
+        champion_ranged = detail_dom('.basic-panel .basic-table tr').eq(0).find('td').eq(1).text()
+        champion_nation = detail_dom('.basic-panel .basic-table tr').eq(2).find('td').eq(1).text()
 
-        image_url = item.find('img.icon').attr('src')
-        image_name = '{}.png'.format(name).lower()
+        image_url = detail_dom.find('.hero-detail-image').attr('src')
+        image_name = 'hero_{}.png'.format(champion_id)
 
-        print("Parsing ", name)
-        try:
-            detail_dom = pq(url=detail_url)
-        except:
-            time.sleep(1)
-            detail_dom = pq(url=detail_url)
-
-        title = detail_dom.find('.god-info .title').text()
-
-        is_range = None
-        nation = None
-        for row in god_rows:
-            cells = row.find('td')
-            if not cells:
-                continue
-
-            name_cell = cells[1]
-
-            if name_cell.text_content().strip().lower() == name.lower():
-                is_range = cells[3].text_content().strip() == 'Ranged'
-                nation = cells[2].text_content().strip()
-                break
-
-        if is_range is None:
-            raise Exception('Invalid `is_range` option.')
-
-        # ullr is both melee & range
-        if name.lower() == 'ullr':
-          is_range = None
-
-        image_path = champion_image_path
-        os.makedirs(image_path, exist_ok=True)
+        skill_nodes = list(detail_dom.find('#hero-abilities').items('li'))
 
         spells = []
-        for spell_dom in detail_dom.items('.single-ability'):
-            skill_image_url = spell_dom.find('img.icon').attr('src')
-            skill_name = spell_dom.find('.ability-name').text()
-            skill_id = '{}_{}'.format(name, skill_name).lower()
+        for skill_node in skill_nodes:
+            skill = json.loads(skill_node.attr('data-ability'))
+
+            skill_name = skill['name']
+            skill_id = skill['id']
+            skill_image_url = skill['icon']
+            skill_image_name = 'skill_{}.png'.format(skill_id)
 
             spells.append({
                 'id': skill_id,
                 'name': skill_name,
                 'image': download_image(
-                    skill_image_url, image_path, '{}.png'.format(skill_id)
+                    skill_image_url, champion_image_path, skill_image_name
                 ),
             })
 
         result.append({
-            'id': name.lower(),
-            'name': name,
-            'title': title,
-            'nation': nation,
-            'image': download_image(
-               image_url, image_path, image_name
+            'id': champion_id,
+            'name': champion_name,
+            'nation': champion_nation,
+            'image': download_champion_image(
+               image_url, champion_image_path, image_name
             ),
-            'is_range': is_range,
+            'is_range': champion_ranged.lower() == 'ranged',
             'spells': spells,
         })
 
     with open('./data/champions.json', 'w') as outfile:
-        json.dump(result, outfile)
+        json.dump(result, outfile, ensure_ascii=False)
 
     return result
 
@@ -164,22 +158,22 @@ def setup_champions():
 def setup_settings():
     result = {
         'ios': {
-            'ad_small': 'ca-app-pub-4764697513834958/5120930069',
-            'ad_big': 'ca-app-pub-4764697513834958/7934795665',
-            'ad_video_id': '1157888',
-            'ad_video_key': '893a78e3ba345e0e38e78418173e44fd988e9ed7',
-            'tracking': 'UA-77793311-8',
-            'store': 'itms-apps://itunes.apple.com/app/id1121065896',
-            'store_premium': 'com.puppybox.quizsmite.premium_version',
+            'ad_small': 'ca-app-pub-4764697513834958/6893120062',
+            'ad_big': 'ca-app-pub-4764697513834958/2183718861',
+            'ad_video_id': '1197471',
+            'ad_video_key': '4c0a685045ec2ea625ac4e00bfd52e894e11b90e',
+            'tracking': 'UA-77793311-2',
+            'store': 'itms-apps://itunes.apple.com/app/id1107274781',
+            'store_premium': 'com.puppybox.quizvg.premium_version',
         },
         'android': {
-            'ad_small': 'ca-app-pub-4764697513834958/5480856869',
-            'ad_big': 'ca-app-pub-4764697513834958/5062054468',
-            'ad_video_id': '1157889',
-            'ad_video_key': 'e88ff04fde322e96808f6350e707775d4027bd7a',
-            'tracking': 'UA-77793311-9',
-            'store': 'market://details?id=com.puppybox.quizsmite',
-            'store_premium': 'com.puppybox.quizsmite.premium_version',
+            'ad_small': 'ca-app-pub-4764697513834958/4637657667',
+            'ad_big': 'ca-app-pub-4764697513834958/5695588466',
+            'ad_video_id': '1197472',
+            'ad_video_key': 'a04ae4e3efe676b70a3f19695b0f95b448e7bb8c',
+            'tracking': 'UA-77793311-3',
+            'store': 'market://details?id=com.puppybox.quizlol',
+            'store_premium': 'com.puppybox.quizvg.premium_version',
         },
         'windows': {
             'ad_small': 'ca-app-pub-4764697513834958/7883646863',
@@ -188,16 +182,15 @@ def setup_settings():
             'ad_video_key': '',
             'tracking': '',
             'store': '',
-            'store_premium': '',
         },
-        'legal_disclaimer': 'This application is not created, sponsored or endorsed by Hi-Rez Studios and doesn’t reflect the views or opinions of Hi-Rez Studios or anyone officially involved in producing or managing SMITE. SMITE is a registered trademark of Hi-Rez Studios. All in-game descriptions, characters, locations, imagery and videos of game content are copyright and are trademarked to their respective owners. Usage for this game falls within fair use guidelines.',
-        'highscore_url': 'http://mobascore-puppybox.rhcloud.com/api/v1/leaderboards/smite/scores/',
-        'source_name': 'SMITE',
-        'source_url': 'http://smitegame.com/',
+        'legal_disclaimer': 'This application is not created, sponsored or endorsed by Super Evil Megacorp and doesn’t reflect the views or opinions of Super Evil Megacorp or anyone officially involved in producing or managing Vainglory. Vainglory is a registered trademark of Super Evil Megacorp. All in-game descriptions, characters, locations, imagery and videos of game content are copyright and are trademarked to their respective owners. Usage for this game falls within fair use guidelines.',
+        'highscore_url': 'http://mobascore-puppybox.rhcloud.com/api/v1/leaderboards/vg/scores/',
+        'source_name': 'Vainglory',
+        'source_url': 'http://www.vainglorygame.com/',
     }
 
     with open('./data/settings.json', 'w') as outfile:
-        json.dump(result, outfile)
+        json.dump(result, outfile, ensure_ascii=False)
 
     return result
 
@@ -297,7 +290,7 @@ def setup_achievements(items, champions):
     ]
 
     with open('./data/achievements.json', 'w') as outfile:
-        json.dump(result, outfile)
+        json.dump(result, outfile, ensure_ascii=False)
 
     return result
 
